955244a68386f5886a10593a8ecc5461ff83ec03
Kill-Joy8 <kill_joy@hotmail.co.uk>
2015-10-16 11:12:47 +0100

    Fixed error pointer so it only appears when needed

--- src/connecting.ml
+++ src/connecting.ml
@@ -9,25 +9,23 @@ let rec print_to_arrow store =
 	| 0 -> printf "^\n"
 	| n -> print_string " "; print_to_arrow (n-1)
 
-let print_err_line lno fileIn =
+let print_err_line lexbuf fileIn =
 	(*print_endline(lexbuf.lex_buffer, lno)*)
-(*	let err = In_channel.create lexbuf.lex_buffer in *)
-	In_channel.seek fileIn (Int64.of_int lno); 
+	let pos = lexbuf.lex_curr_p in
+	In_channel.seek fileIn (Int64.of_int pos.pos_bol); 
 	match In_channel.input_line ?fix_win_eol:(Some true) fileIn with
-		| Some line -> print_endline(line)
+		| Some line -> print_endline(line); print_to_arrow (pos.pos_cnum - pos.pos_bol-1)
 		| None -> ()
 
 let print_pos out lexbuf =
 	let pos = lexbuf.lex_curr_p in
 	fprintf out "%s:%d:%d" pos.pos_fname
 (* Print out current error item being lexed *)
-	 pos.pos_lnum (pos.pos_cnum - pos.pos_bol +1); print_to_arrow (pos.pos_cnum - pos.pos_bol-1)
-
-(* printf "%s" (lexbuf.lex_buffer)*)
+	 pos.pos_lnum (pos.pos_cnum - pos.pos_bol +1)
 
 let parse_error lexbuf fileIn = 
 	try Parser.prog Lexer.main lexbuf with
-	| SyntaxError msg -> print_err_line (lexbuf.lex_curr_p.pos_bol) fileIn; fprintf stderr  "%a: %s\n" print_pos lexbuf msg; exit(-1)
+	| SyntaxError msg -> print_err_line lexbuf fileIn; fprintf stderr  "%a: %s\n" print_pos lexbuf msg; exit(-1)
 	| Parser.Error -> fprintf stderr "%a: Syntax Error\n" print_pos lexbuf; exit(-1)
 
 let rec parse_print lexbuf fileIn = (* Add error handling to here *)
